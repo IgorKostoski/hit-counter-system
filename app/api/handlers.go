c3c054b2f88501fef9ac60b7201889832f0e36fa
Igor Kostoski <igorkostoski@ymail.com>
2025-05-24 22:10:50 +0200

    feat: Add basic GitHub Actions CI for Go API build and test

--- app/api/handlers.go
+++ app/api/handlers.go
@@ -7,13 +7,13 @@ import (
 )
 
 type HitRequest struct {
-	Key string `json:"key binding:"required"`
+	Key string `json:"key" binding:"required"`
 }
 
 func HitHandler(c *gin.Context) {
 	var req HitRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request " + err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request: " + err.Error()})
 		return
 	}
 	if req.Key == "" {
@@ -28,7 +28,7 @@ func HitHandler(c *gin.Context) {
 		return
 	}
 
-	hitsProcessed.WithLabelValues("succes").Inc()
+	hitsProcessed.WithLabelValues("success").Inc()
 	c.JSON(http.StatusOK, gin.H{"key": req.Key, "new_count": count})
 }
 
